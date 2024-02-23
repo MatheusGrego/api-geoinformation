b2e25d0a70f95c09018cdd017a6035dc02bbdcf5
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2024-02-23 17:30:20 -0300

    refactor - pom.xml

---
+++ src/main/java/com/apigeoinformation/ApiGeoinformationApplication.java
@@ -0,0 +1,13 @@
+package com.apigeoinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGeoinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGeoinformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apigeoinformation/ApiGeoinformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigeoinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGeoinformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
